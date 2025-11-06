cad2fc89f2cb6f9f33f63f203d118837c73adcc9
ProphetOfPastafarianism <magnushuston123@gmail.com>
2025-11-06 19:27:36 +0000

    FINISH

--- Pattern.java
+++ Pattern.java
@@ -1,10 +1,15 @@
 public class Pattern{
-
+//magnus november 6th 2025 period 7
+//declaring my variables
 int rows;
 int start;
 int maxE;
     public static void stars(int rows){
+        //Pre-condition: rows must be a number and not negative
+          // dont do a super duper long equation as the input number
+        //Post-condition: it will print out the rows of stars until it reaches the number you put in.
         int i = 0;
+        //make the word to print
         String starstr = "*";
         while (rows>=i){
             System.out.println(starstr.repeat(i));
@@ -12,6 +17,10 @@ int maxE;
         }
     }
     public static void triangle(int rows){
+          //Pre-condition: rows must be a number and not negative
+          // dont do a super duper long equation as the input number
+        //Post-condition: it will print out the number it is and it will do that the same ammount of times as it is a number until it reaches the number you put in.
+
          int i = 1;
       
         while (rows>=i){
@@ -21,21 +30,33 @@ int maxE;
         }
     }
     public static void odds(int start){
+        //Pre-condition: start must be a number and not negative
+          // dont do a super duper long equation as the input number
+        //Post-condition: it will print out the rows of start until it reaches a number equal or less than 1.
         for (int i = start; i>=0; i-=2){
               String startt = ""+i;
             System.out.println(startt.repeat(i));
         }
     }
     public static void eo(int maxE){
-        String e = "e";
-            String o = "o";
+        //Pre-condition: rows must be a number and not negative
+          // dont do a super duper long equation as the input number
+        //Post-condition: it will print out e and o until it reaches the max number you gave it, then it will go down from there until it reaches 1.
+        String e = "o";
+            String o = "e";
             int i = 0;
-    while(i<maxE){
+            int ogmaxE=maxE;
+    while(i<ogmaxE){
+        if (maxE%2==0){
+        maxE++;
         i++;
         System.out.println(e.repeat(i));
+        }
+        if (maxE%2!=0){
+        maxE++;
         i++;
         System.out.println(o.repeat(i));
-     
+        }
     }
     while (i>0) {
         i--;
@@ -47,14 +68,29 @@ int maxE;
     }
 }
     public static void pyramid(int rows){
+        //Pre-condition: rows must be a number and not negative
+          // dont do a super duper long equation as the input number
+        //Post-condition: it will print out a pyramid of the numbers given from greatest at the top and least at the bottom
+        int x =1;
+        int ogrows = rows;
+       
+        String space = " ";
+        while (x<=ogrows){
+            String num = ""+(rows);
+          int eviltwo=rows*2-1;
+            System.out.println((space.repeat(x))+(num.repeat(eviltwo)));
+            rows--;
+            x++;
+
+        }
 
     }
 
         public static void main(String[] args){
             stars(6);
-            triangle(5);
+            triangle(7);
             odds(9);
-            eo(5);
+            eo(6);
             pyramid(6);
         }
 
